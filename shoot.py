ddd2cb6d562d68294261098f4186fa9106b4400e
Massanobu <otaviomassanobu@hotmail.com>
2024-09-05 10:10:25 -0300

    Corrigido erro na função de login e atualizado o README

--- shoot.py
+++ shoot.py
@@ -1,24 +1,5 @@
 import pygame
 
-#shoot normal
-'''class Shoot(pygame.sprite.Sprite):
-    def __init__(self,pos,speed,screen_height):
-        super().__init__()
-        self.image = pygame.Surface((4,10))
-        self.image.fill('black')
-        self.rect = self.image.get_rect(center = pos)
-        self.speed = speed
-        self.height_y_constraint = screen_height
-
-    def destroy(self): #destrruir os shoots quando sair da tela
-        if self.rect.y <= -50 or self.rect.y >= self.height_y_constraint + 50:
-            self.kill()
-
-    def update(self): #movimenta o objeto retangulo shoot pra cima no eixo y em uma certa velocidade
-        self.rect.y += self.speed
-        self.destroy()
-'''
-#shoot buffado
 class Shoot(pygame.sprite.Sprite):
     def __init__(self, pos, speed, screen_height, big_bullet=False):
         super().__init__()
